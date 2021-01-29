99b3ab102b6b22875f9dc14ac376e4dbd16e9a6a
wtfoxe <68599257+wtfoxe@users.noreply.github.com>
2021-01-29 16:33:00 -0300

    final da primeira aula

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+//import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+
+//const BackgroundImage = styled.div`
+//  background-image: url(${db.bg});
+//  flex: 1;
+//  background-size: cover;
+//  /*background-position: center;*/
+//  height: 925px;
+//`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage = {db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The Legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner />
+    </QuizBackground>
+  );
+}
